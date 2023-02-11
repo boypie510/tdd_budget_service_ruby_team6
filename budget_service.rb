c1cb6d1d029e27b5f1bcf3df188c25ba4d72eabe
boypie510 <boypie510@gmail.com>
2023-02-12 00:32:39 +0800

    refactor organize

--- budget_service.rb
+++ budget_service.rb
@@ -8,16 +8,12 @@ class BudgetService
 
   def query(start_date, end_date)
     array_of_budget = BudgetRepo.new.get_all
-    budget_hash = {}
-    array_of_budget.map do |budget|
-      budget_hash[budget.year_month] = budget.amount
-    end
+    budget_hash = generate_budget_hash(array_of_budget)
 
     if start_date.month == end_date.month
       budget_hash[start_date.strftime('%Y%m')] * ratio(start_date, end_date)
     elsif end_date.month != start_date.month
-      budget_hash[start_date.strftime('%Y%m')] * ratio(start_date, last_day_in_month(start_date)) +
-        budget_hash[end_date.strftime('%Y%m')] * ratio(first_day_in_month(end_date), end_date) +
+      start_date_month_budget(start_date, budget_hash) + end_date_month_budget(end_date, budget_hash) +
         whole_month_budget_between(start_date, end_date, budget_hash)
     end
   end
@@ -26,8 +22,8 @@ class BudgetService
 
   def ratio(start_date, end_date)
     partial_day = (end_date - start_date).to_i + 1
-    days_in_month = last_day_in_month(start_date).day.to_f
-    partial_day / days_in_month
+    days_in_month = last_day_in_month(start_date).day
+    partial_day / days_in_month.to_f
   end
 
   def last_day_in_month(start_date)
@@ -47,5 +43,22 @@ class BudgetService
     end
 
     sum
+  end
+
+  def generate_budget_hash(array_of_budget)
+    hash = {}
+    array_of_budget.map do |budget|
+      hash[budget.year_month] = budget.amount
+    end
+
+    hash
+  end
+
+  def start_date_month_budget(start_date, budget_hash)
+    budget_hash[start_date.strftime('%Y%m')] * ratio(start_date, last_day_in_month(start_date))
+  end
+
+  def end_date_month_budget(end_date, budget_hash)
+    budget_hash[end_date.strftime('%Y%m')] * ratio(first_day_in_month(end_date), end_date)
   end
 end
